94ef1a0703ebe1723ae968c5793e1bee6a5cf438
Dương TH <78488263+duongth201@users.noreply.github.com>
2022-02-05 00:32:01 +0700

    first

---
+++ src/java/DAL/HumanDAO.java
@@ -0,0 +1,121 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package DAL;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Humans;
+
+/**
+*
+* @author fsoft
+*/
+public class HumanDAO extends BaseDAO<Humans> {
+
+   @Override
+   public ArrayList<Humans> getStudents() {
+       ArrayList<Humans> students = new ArrayList<>();
+       try {
+           String sql = "SELECT [id]\n" +
+                   "      ,[name]\n" +
+                   "      ,[gender]\n" +
+                   "      ,[dob]\n" +
+                   "  FROM [Student]";
+           PreparedStatement statement = connection.prepareStatement(sql);
+           ResultSet rs = statement.executeQuery();
+           while(rs.next())
+           {
+               Humans s = new Humans();
+               s.setId(rs.getInt("id"));
+               s.setName(rs.getString("name"));
+               s.setGender(rs.getBoolean("gender"));
+               s.setDob(rs.getDate("dob"));
+               students.add(s);
+           }
+       } catch (SQLException ex) {
+           Logger.getLogger(HumanDAO.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       return students;
+   }
+   public Humans getStudent(int id) {
+       try {
+           String sql = "SELECT s.id,s.name,s.gender,s.dob FROM Student s\n"
+                   + "WHERE s.id = ?";
+           PreparedStatement statement = connection.prepareStatement(sql);
+           statement.setInt(1, id);
+           ResultSet rs = statement.executeQuery();
+           if (rs.next()) {
+               Humans s = new Humans();
+               s.setId(rs.getInt("id"));
+               s.setName(rs.getString("name"));
+               s.setDob(rs.getDate("dob"));
+               s.setGender(rs.getBoolean("gender"));
+               return s;
+           }
+
+       } catch (SQLException ex) {
+           Logger.getLogger(HumanDAO.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       return null;
+   }
+
+
+   public void insertStudent(Humans s) {
+       try {
+           String sql = "INSERT INTO [Student](\n"
+                   + "           [name]\n"
+                   + "           ,[dob]\n"
+                   + "           ,[gender])\n"
+                   + "     VALUES(\n"
+                   + "           ?\n"
+                   + "           ,?\n"
+                   + "           ,?)";
+           PreparedStatement statement = connection.prepareStatement(sql);
+//           statement.setInt(1, s.getId());
+           statement.setString(1, s.getName());
+           statement.setDate(2, (Date) s.getDob());
+           statement.setBoolean(3, s.isGender());
+           statement.executeUpdate();
+       } catch (SQLException ex) {
+           Logger.getLogger(HumanDAO.class.getName()).log(Level.SEVERE, null, ex);
+       }
+   }
+   
+   public void updateStudent(Humans s) {
+       try {
+           String sql = "UPDATE [Student]\n"
+                   + "   SET [name] = ?\n"
+                   + "      ,[dob] = ?\n"
+                   + "      ,[gender] = ?\n"
+                   + " WHERE [id] = ?";
+           PreparedStatement statement = connection.prepareStatement(sql);
+           statement.setString(1, s.getName());
+           statement.setDate(2, new java.sql.Date(s.getDob().getTime()));
+           statement.setBoolean(3, s.isGender());
+           statement.setInt(4, s.getId());
+           statement.executeUpdate();
+       } catch (SQLException ex) {
+           Logger.getLogger(HumanDAO.class.getName()).log(Level.SEVERE, null, ex);
+       }
+   }
+
+   public void deleteStudent(int id) {
+       try {
+           String sql = "DELETE Student WHERE id=?";
+           PreparedStatement statement = connection.prepareStatement(sql);
+           statement.setInt(1, id);
+           statement.executeUpdate();
+       } catch (SQLException ex) {
+           Logger.getLogger(HumanDAO.class.getName()).log(Level.SEVERE, null, ex);
+       }
+   }
+
+}
---
+++ src/java/controller/HumanCreate.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import DAL.HumanDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Humans;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class HumanCreate extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("../add.jsp").forward(request, response);
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        SimpleDateFormat spf = new SimpleDateFormat("dd/MM/yyyy");
+        boolean gender;
+        String name = request.getParameter("name");
+        if (request.getParameter("gender").equalsIgnoreCase("Male")) {
+            gender = true;
+        } else {
+            gender = false;
+        }
+        String date = request.getParameter("date");
+        Date dob = null;
+        try {
+            dob = spf.parse(date);
+        } catch (ParseException ex) {
+            response.sendRedirect("create");
+        }
+        java.sql.Date sqlDate = new java.sql.Date(dob.getTime());
+
+        Humans s = new Humans(name, gender, sqlDate);
+        HumanDAO sdao = new HumanDAO();
+        sdao.insertStudent(s);
+
+        response.sendRedirect("list");
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
